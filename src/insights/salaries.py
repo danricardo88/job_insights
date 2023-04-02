55308a7466a625090a078d9fb7381056503d5cce
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-02 13:13:59 -0300

    req-4

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,4 +1,5 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
@@ -16,6 +17,26 @@ def get_max_salary(path: str) -> int:
     int
         The maximum salary paid out of all job opportunities
     """
+    data = read(path)
+    max_salary = None
+    # para cada linha de emprego nos dados carregados
+    for row in data:
+        # obtém o valor de max_salary da linha, caso exista
+        salary = row.get("max_salary")
+        # verifica se o valor de max_salary é diferente de None
+        if salary:
+            # tenta converter o valor de max_salary para um inteiro
+            try:
+                salary = int(salary)
+                """ se max_salary ainda não tiver definido ou se
+                o novo valor for maior que o anterior, atualiza max_salary"""
+                if max_salary is None or salary > max_salary:
+                    max_salary = salary
+                """ ignora exceções de ValueError, q pode ser lançadas
+                caso max_salary não possa ser convertido para inteiro """
+            except ValueError:
+                pass
+    return max_salary
     raise NotImplementedError
 
 
