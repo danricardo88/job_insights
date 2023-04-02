3f6184194a1741b6668441ad966fe81e8c6b6846
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-02 17:12:22 -0300

    tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,23 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """Testa se a contagem de palavras é feita
+    corretamente para a palavra 'Python' em letras minúsculas"""
+    count = count_ocurrences("data/jobs.csv", "python")
+    assert count == 1639
+
+    """Testa se a contagem de palavras é feita corretamente
+     para a palavra 'Python' em letras maiúsculas"""
+    count = count_ocurrences("data/jobs.csv", "PYTHON")
+    assert count == 1639
+
+    """Testa se a contagem de palavras é feita corretamente
+    para a palavra 'Javascript' em letras minúsculas"""
+    count = count_ocurrences("data/jobs.csv", "javascript")
+    assert count == 1190
+
+    """Testa se a contagem de palavras é feita corretamente
+    para a palavra 'Javascript' em letras maiúsculas"""
+    count = count_ocurrences("data/jobs.csv", "JAVASCRIPT")
+    assert count == 1190
