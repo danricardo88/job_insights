f316b566fc1af068b9db24171a41565fd7dbe559
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-02 13:31:12 -0300

    req-4 acho que não vai passar.... nojo de py

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -18,24 +18,14 @@ def get_max_salary(path: str) -> int:
         The maximum salary paid out of all job opportunities
     """
     data = read(path)
-    max_salary = None
-    # para cada linha de emprego nos dados carregados
-    for row in data:
-        # obtém o valor de max_salary da linha, caso exista
-        salary = row.get("max_salary")
-        # verifica se o valor de max_salary é diferente de None
-        if salary:
-            # tenta converter o valor de max_salary para um inteiro
-            try:
-                salary = int(salary)
-                """ se max_salary ainda não tiver definido ou se
-                o novo valor for maior que o anterior, atualiza max_salary"""
-                if max_salary is None or salary > max_salary:
-                    max_salary = salary
-                """ ignora exceções de ValueError, q pode ser lançadas
-                caso max_salary não possa ser convertido para inteiro """
-            except ValueError:
-                pass
+    # utiliza list comprehension para criar uma lista de salários não nulos
+    salaries = [
+        int(row["max_salary"])
+        for row in data
+        if row.get("max_salary")
+    ]
+    # utiliza a função max() do Python para obter o maior salário da lista
+    max_salary = max(salaries)
     return max_salary
     raise NotImplementedError
 
