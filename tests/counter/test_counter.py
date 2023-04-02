a9db53fef4a81acc0d803cca1dbe0b9e0bddb118
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-02 17:29:35 -0300

    tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,22 +2,27 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    """Testa se a contagem de palavras é feita
-    corretamente para a palavra 'Python' em letras minúsculas"""
-    count = count_ocurrences("data/jobs.csv", "python")
-    assert count == 1639
 
-    """Testa se a contagem de palavras é feita corretamente
-     para a palavra 'Python' em letras maiúsculas"""
-    count = count_ocurrences("data/jobs.csv", "PYTHON")
-    assert count == 1639
+    # Teste para a palavra "Python"
+    camel_case = count_ocurrences("data/jobs.csv", "Python")
+    assert camel_case == 1639
+    uppercase = count_ocurrences("data/jobs.csv", "PYTHON")
+    assert uppercase == 1639
+    lowercase = count_ocurrences("data/jobs.csv", "python")
+    assert lowercase == 1639
 
-    """Testa se a contagem de palavras é feita corretamente
-    para a palavra 'Javascript' em letras minúsculas"""
-    count = count_ocurrences("data/jobs.csv", "javascript")
-    assert count == 1190
+    # Teste para a palavra "Javascript"
+    camel_case = count_ocurrences("data/jobs.csv", "Javascript")
+    assert camel_case == 1139
+    uppercase = count_ocurrences("data/jobs.csv", "JAVASCRIPT")
+    assert uppercase == 1139
+    lowercase = count_ocurrences("data/jobs.csv", "javascript")
+    assert lowercase == 1139
 
-    """Testa se a contagem de palavras é feita corretamente
-    para a palavra 'Javascript' em letras maiúsculas"""
-    count = count_ocurrences("data/jobs.csv", "JAVASCRIPT")
-    assert count == 1190
+    # Teste para a palavra "Ruby"
+    camel_case = count_ocurrences("data/jobs.csv", "Ruby")
+    assert camel_case == 297
+    uppercase = count_ocurrences("data/jobs.csv", "RUBY")
+    assert uppercase == 297
+    lowercase = count_ocurrences("data/jobs.csv", "ruby")
+    assert lowercase == 297
