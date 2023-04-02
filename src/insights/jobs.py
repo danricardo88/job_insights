5c38ce015566f1c863aa6fbad9bde24b771fddc6
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-02 12:41:55 -0300

    req-2 outra tentativa

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -4,14 +4,12 @@ import csv
 
 
 @lru_cache
-def read_file(path: str) -> List[Dict]:
+def read(path: str) -> List[Dict]:
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
@@ -20,38 +18,35 @@ def read_file(path: str) -> List[Dict]:
     with open(path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         return [row for row in reader]
+    raise NotImplementedError
 
 
 def get_unique_job_types(path: str) -> List[str]:
     """Checks all different job types and returns a list of them
-
-    Must call `read_file`
-
+    Must call `read`
     Parameters
     ----------
     path : str
-        Must be passed to `read_file`
-
+        Must be passed to `read`
     Returns
     -------
     list
         List of unique job types
     """
-    data = read_file(path)
-    unique_job_types = set(row["job_type"] for row in data if row["job_type"])
-    return list(unique_job_types)
+    data = read(path)
+    job_types = {row['job_type'] for row in data if row['job_type']}
+    return list(job_types)
+    raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     """Filters a list of jobs by job_type
-
     Parameters
     ----------
     jobs : list
         List of jobs to be filtered
     job_type : str
         Job type for the list filter
-
     Returns
     -------
     list
